0a2522d757befb1eeb89b00cf555a1bf42d8eca6
Hasmukh Gehlot <hasmukh.gehlot4@gmail.com>
2023-02-07 11:56:21 +0530

    commit 3 - json file read successfull

--- commit.md
+++ commit.md
@@ -1,1 +1,2 @@
 commit 2 - added the commit file
+commit 3 - json file read successfull
--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Json from a File</h1>
+        {data.map((post) => (
+            <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            </div>
+        ))}
       </header>
     </div>
   );
